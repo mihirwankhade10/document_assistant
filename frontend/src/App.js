cf0bb8f710e2128c5c6dac2b27c62ed5c910cc0a
Mihir Wankhade <abhiwankhaded67@gmail.com>
2023-09-16 18:25:51 +0530

    created a navbar today

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import BasicExample from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <BasicExample />
+    </div>
+  );
+}
+
+export default App;
